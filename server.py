3f11d6bbd57747517f4f14ee5efb04f7350ff780
Guilleag01 <guilleag01@gmail.com>
2023-03-25 18:43:07 +0100

    movil inicial

--- server.py
+++ server.py
@@ -3,18 +3,9 @@ import os
 
 app = Flask(__name__)
 
-@app.route('/subir-archivo', methods=['POST'])
+@app.route('/get_new_question', methods=['GET'])
 def subir_archivo():
-    for filename in os.listdir('./'):
-        if os.path.splitext(filename)[0] == 'video':
-            os.remove(filename)
-    archivo = request.files['archivo']
-    ruta_archivo = './video.' + archivo.filename.split('.')[-1]
-    archivo.save(ruta_archivo)
-    app.logger.info("Enviando")
-    ret = send_file('base.txt', as_attachment=True)
-    app.logger.info("Enviado!")
-    return ret
+    print
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
